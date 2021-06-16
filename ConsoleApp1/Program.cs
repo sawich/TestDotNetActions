46ba349ceddc1aaee04cfee7f91d953a5c13ed28
sawich <sawich@angeldev.org>
2021-06-16 05:50:20 +0300

    -

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Version: {0}", System.Environment.Version.ToString());
         }
     }
 }
