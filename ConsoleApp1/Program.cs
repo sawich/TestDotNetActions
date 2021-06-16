9e8ec29f6a2dd5989dc5c09c025a43f39f54e67f
sawich <sawich@angeldev.org>
2021-06-16 05:17:29 +0300

    -

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
